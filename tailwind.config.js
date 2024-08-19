d07b68f84589ff1e925b5165be5e19f287b702b7
Guilherme <guilhermeendonca1997@gmail.com>
2024-08-19 09:44:46 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Arquivos a serem escaneados para classes do Tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
